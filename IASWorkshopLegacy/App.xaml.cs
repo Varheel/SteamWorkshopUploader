dab9e2d9905562198dddef489604168084a872db
Varheel <varheely@gmail.com>
2022-12-02 01:20:07 -0600

    Implement "Legacy" version of this project
    - Change solution structure (projects in separate subfolders)
    - Create IASWorkshopLegacy project
    - Target .NET Framework 3.5
    - Use the Steamworks.NET library instead of Facepunch.Steamworks

---
+++ IASWorkshopLegacy/App.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Steamworks;
+using System;
+using System.Threading;
+using System.Windows;
+
+namespace IASWorkshopLegacy
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// The AppId for I Am Sakuya
+        /// </summary>
+        public static readonly AppId_t AppId = new AppId_t(1960590);
+
+        private static bool _running = true;
+
+        /// <summary>
+        /// WPF calls this method when the app starts up, but before the first window appears.
+        /// </summary>
+        /// <param name="e">Contains the program's command-line arguments</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            bool success;
+
+            // Steam will look for the App ID during initialization
+            Environment.SetEnvironmentVariable("SteamAppId", AppId.ToString());
+            Environment.SetEnvironmentVariable("SteamGameId", AppId.ToString());
+
+            try
+            {
+                success = SteamAPI.Init();
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+
+            if (success)
+            {
+                ThreadPool.QueueUserWorkItem(RunCallbacks);
+            }
+            else
+            {
+                MessageBox.Show("Could not connect to Steam. Make sure Steam is running and try again.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Shutdown();
+            }
+        }
+
+        /// <summary>
+        /// Runs Steam callbacks repeatedly until the program exits.
+        /// </summary>
+        private static void RunCallbacks(object state)
+        {
+            while (_running)
+            {
+                Current.Dispatcher.Invoke(new Action(SteamAPI.RunCallbacks));
+                Thread.Sleep(100); // Every 100 ms, or 10 times per second
+            }
+        }
+
+        /// <summary>
+        /// WPF calls this when the app is about to exit.
+        /// </summary>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _running = false;
+            SteamAPI.Shutdown();
+            base.OnExit(e);
+        }
+    }
+}
