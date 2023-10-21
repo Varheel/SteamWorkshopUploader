14d05ad8f134ae7b15c6e6e1004add7a47f9ac83
Varheel <varheely@gmail.com>
2023-10-20 22:03:26 -0500

    Hopefully fix antivirus issue
    - The "legacy" project should work on most Windows editions. The main project could probably be deleted.
    - Create separate 32-bit and 64-bit build configurations
    - Make the user press a button before reading the App ID and connecting to Steam

--- SteamWorkshopUploaderLegacy/App.xaml.cs
+++ SteamWorkshopUploaderLegacy/App.xaml.cs
@@ -1,7 +1,4 @@
 ﻿using Steamworks;
-using System;
-using System.IO;
-using System.Threading;
 using System.Windows;
 
 namespace SteamWorkshopUploaderLegacy
@@ -14,101 +11,24 @@ namespace SteamWorkshopUploaderLegacy
         /// <summary>
         /// The number that identifies the game on the Steam Workshop.
         /// </summary>
-        public static AppId_t AppId { get; private set; }
-
-        private const string AppIdFile = "AppID.txt";
-        private const string FileError = "Make sure AppID.txt contains a valid number.";
-        private const string ConnectionError = "Make sure Steam is running and your App ID is correct.";
-
-        private static bool _running = true;
+        public static AppId_t AppId { get; set; }
 
         /// <summary>
-        /// WPF calls this method when the app starts up, but before the first window appears.
+        /// True while the Steam API is running.
         /// </summary>
-        /// <param name="e">Contains the program's command-line arguments</param>
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-
-            string error = null;
-
-            // Read the game's App ID from the included text file
-            try
-            {
-                foreach (string line in File.ReadAllLines(AppIdFile))
-                {
-                    // Split on whitespace (the default for Split) and take the first element (Split never returns an empty array)
-                    string first = line.Split()[0];
-
-                    if (uint.TryParse(first, out uint appId))
-                    {
-                        AppId = new AppId_t(appId);
-                        // Steam will look for the App ID in env vars during initialization
-                        Environment.SetEnvironmentVariable("SteamAppId", appId.ToString());
-                        Environment.SetEnvironmentVariable("SteamGameId", appId.ToString());
-                        break;
-                    }
-                }
-
-                // Did we find a number?
-                if (AppId.m_AppId == 0)
-                {
-                    error = FileError;
-                }
-            }
-            catch (Exception ex)
-            {
-                error = FileError;
-            }
-
-            // Initialize the Steam API
-            if (error == null)
-            {
-                try
-                {
-                    if (SteamAPI.Init())
-                    {
-                        ThreadPool.QueueUserWorkItem(RunCallbacks);
-                    }
-                    else
-                    {
-                        error = ConnectionError;
-                    }
-                }
-                catch
-                {
-                    error = ConnectionError;
-                }
-            }
-
-            // If there was an error, show a message and quit
-            if (error != null)
-            {
-                MessageBox.Show(error, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                Shutdown();
-            }
-        }
-
-        /// <summary>
-        /// Runs Steam callbacks repeatedly until the program exits.
-        /// </summary>
-        private static void RunCallbacks(object state)
-        {
-            while (_running)
-            {
-                Current.Dispatcher.Invoke(new Action(SteamAPI.RunCallbacks));
-                Thread.Sleep(100); // Every 100 ms, or 10 times per second
-            }
-        }
+        public static bool Running { get; set; } = false;
 
         /// <summary>
         /// WPF calls this when the app is about to exit.
         /// </summary>
         protected override void OnExit(ExitEventArgs e)
         {
-            _running = false;
-            SteamAPI.Shutdown();
-            base.OnExit(e);
+            if (Running)
+            {
+                Running = false;
+                SteamAPI.Shutdown();
+                base.OnExit(e);
+            }
         }
     }
 }
